44fde56ddca4b1973585c0a48a5dfcd5c51a2c4b
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-11-02 20:37:42 -0400

    Now supports polar vectors

--- forward_kinematics.py
+++ forward_kinematics.py
@@ -2,6 +2,7 @@
 Find first vector and for next vectors add angle to first one and it should work
 """
 import pygame
+import math
 
 pygame.init()
 
@@ -17,6 +18,12 @@ def draw_cvector(screen, tail, head):
 def sum_vector(A, B):
     return (A[0] + B[0], A[1] + B[1])
 
+def convert_cartesian(A):
+    # A = (mag, angle)
+    angle = (A[1] * math.pi) / 180
+    mag = A[0]
+    return (mag * math.cos(angle), mag * math.sin(angle))
+
 # Run until the user asks to quit
 running = True
 while running:
@@ -31,26 +38,17 @@ while running:
 
     # segements of arm
     vector_array = []
-    vector_array = [(50,50), (0, 50), (-50, 50)]
+    vector_array = [(50,45), (50, -45), (50, 45)] # polar vectors
 
     # draw full arm
     cum_vector = [0,0] # cumulative vector
     for vector in vector_array:
-        draw_cvector(screen, cum_vector, sum_vector(cum_vector, vector))
-        cum_vector = sum_vector(cum_vector, vector)
+        v1 = convert_cartesian(vector)
+        draw_cvector(screen, cum_vector, sum_vector(cum_vector, v1))
+        cum_vector = sum_vector(cum_vector, v1)
 
     # draw end point
     draw_cvector(screen, (0,0), cum_vector)
-
-    """
-    # demo stuff
-    v1 = (100, 100)
-    v1 = sum_vector(v1, (100,20))
-    draw_cvector(screen, (0, 0), v1)
-
-    draw_cvector(screen, (0, 0), (100, 100))
-    draw_cvector(screen, (100, 100), (100+100,20+100))
-    """
 
     # Flip the display
     pygame.display.flip()
